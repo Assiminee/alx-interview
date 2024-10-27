98302107c1496313b9832d55715b2fd6f572bf0f
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-27 23:07:46 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,29 +4,30 @@ Prints metrics based on input from stdin
 """
 import sys
 
-stats = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total, counter = 0, 0
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 
 def print_metrics():
-    """Prints the metrics"""
-    print(f'File size: {total}')
-
-    for key, value in sorted(stats.items()):
+    '''Prints the metrics'''
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes.items()):
         if value != 0:
-            print(f'{key}: {value}')
+            print('{}: {}'.format(key, value))
 
 
 try:
     for line in sys.stdin:
         parts = line.split()
-        if len(parts) == 9:
+        if len(parts) > 4:
             try:
-                code, size = parts[-2], int(parts[-1])
-                if code in stats:
-                    stats[code] += 1
-                total += size
+                code = parts[-2]
+                size = int(parts[-1])
+                if code in status_codes:
+                    status_codes[code] += 1
+                total_size += size
                 counter += 1
             except (IndexError, ValueError):
                 continue
@@ -36,4 +37,7 @@ try:
             counter = 0
 
 except KeyboardInterrupt:
+    print_metrics()
     sys.exit(0)
+
+print_metrics()
