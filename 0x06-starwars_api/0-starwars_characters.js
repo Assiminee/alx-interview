34b19a1318dcf17505e9678674726344cbea687c
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-15 01:20:13 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,45 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, data) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(data).name);
-          }
+function getCharacters(movieId) {
+    const url = `https://swapi.dev/api/films/${movieId}/`;
+
+    request(url, (err, response, body) => {
+        if (err) {
+            console.error('Error fetching movie:', err);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            console.error(`Failed to retrieve movie data: ${response.statusCode}`);
+            return;
+        }
+
+        const movieData = JSON.parse(body);
+
+        movieData.characters.forEach(characterUrl => {
+            request(characterUrl, (err, response, body) => {
+                if (err) {
+                    console.error('Error fetching character:', err);
+                    return;
+                }
+
+                if (response.statusCode !== 200) {
+                    console.error(`Failed to retrieve character data: ${response.statusCode}`);
+                    return;
+                }
+
+                const characterData = JSON.parse(body);
+                console.log(characterData.name);
+            });
         });
-      });
-      console.log(res);
-    }
-  }
-});
+    });
+}
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+    console.log("Usage: node starwars_characters.js <movie_id>");
+} else {
+    const movieId = args[0];
+    getCharacters(movieId);
+}
