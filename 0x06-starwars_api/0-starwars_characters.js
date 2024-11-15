c4e4bdca761312b1b0e495df79113295231256d4
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-15 01:35:07 +0100

    test

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,33 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getCharacters(movieId) {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
-
-    request(url, (err, response, body) => {
-        if (err) {
-            console.error('Error fetching movie:', err);
-            return;
-        }
-
-        if (response.statusCode !== 200) {
-            console.error(`Failed to retrieve movie data: ${response.statusCode}`);
-            return;
-        }
-
-        const movieData = JSON.parse(body);
-
-        movieData.characters.forEach(characterUrl => {
-            request(characterUrl, (err, response, body) => {
-                if (err) {
-                    console.error('Error fetching character:', err);
-                    return;
-                }
-
-                if (response.statusCode !== 200) {
-                    console.error(`Failed to retrieve character data: ${response.statusCode}`);
-                    return;
-                }
-
-                const characterData = JSON.parse(body);
-                console.log(characterData.name);
-            });
+function requestPromise(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (err, response, body) => {
+            if (err)
+                reject(err);
+            else
+                resolve({ response, body });
         });
     });
 }
 
-const args = process.argv.slice(2);
-if (args.length !== 1) {
-    console.log("Usage: node starwars_characters.js <movie_id>");
-} else {
-    const movieId = args[0];
-    getCharacters(movieId);
+async function getCharacters(movieId) {
+    try {
+        const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+        const movieResponse = await requestPromise(url);
+        const movieData = JSON.parse(movieResponse.body);
+
+        for (const characterUrl of movieData.characters) {
+            const characterResponse = await requestPromise(characterUrl);
+            const characterData = JSON.parse(characterResponse.body);
+            console.log(`${characterData.name}`);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
+
+const args = process.argv.slice(2);
+const movieId = args[0];
+getCharacters(movieId);
